5073e74b62107216a60bc9f480c2b6529139cd79
onepiece998 <manusingh09061994@gmail.com>
2020-07-10 18:07:35 -0800

    firebase

--- lib/Screens/LoginScreen.dart
+++ lib/Screens/LoginScreen.dart
@@ -1,85 +1,117 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/HomePage.dart';
 
 class LoginScreen extends StatelessWidget {
+  final _emailController = TextEditingController();
+  final _passController = TextEditingController();
+
+  // Future<FirebaseUser> login(String email, String pass) async {
+  //   FirebaseAuth _auth = FirebaseAuth.instance;
+  //   try {
+  //     AuthResult result = await _auth.createUserWithEmailAndPassword(
+  //         emai: email, password: pass);
+  //     FirebaseUser user = result.User;
+  //     return user;
+  //   } catch (e) {
+  //     print(e);
+  //     return null;
+  //   }
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-              child: SingleChildScrollView(
-            child: Container(
-              padding: EdgeInsets.all(32),
-              child: Form(
-          child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text("Registration",
-              style: TextStyle(
-                color: Colors.lightBlue,
-                fontSize: 36,
-                fontWeight: FontWeight.bold,
-              )),
-          SizedBox(
-            height: 16,
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.transparent),
-                    borderRadius: BorderRadius.all(Radius.circular(15.0))),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.greenAccent),
-                  borderRadius: BorderRadius.all(Radius.circular(15.0)),
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(32),
+            child: Form(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text("Registration",
+                    style: TextStyle(
+                      color: Colors.lightBlue,
+                      fontSize: 36,
+                      fontWeight: FontWeight.bold,
+                    )),
+                SizedBox(
+                  height: 16,
                 ),
-                prefixIcon: Icon(
-                  Icons.email,
-                  size: 30.0,
+                TextFormField(
+                   controller: _emailController,
+                  decoration: InputDecoration(
+                   
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.transparent),
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(15.0))),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.greenAccent),
+                        borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                      ),
+                      prefixIcon: Icon(
+                        Icons.email,
+                        size: 30.0,
+                      ),
+                      hintText: "Email",
+                      filled: true,
+                      fillColor: Colors.grey[200]),
                 ),
-                hintText: "Email",
-                filled: true,
-                fillColor: Colors.grey[200]),
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.transparent),
-                    borderRadius: BorderRadius.all(Radius.circular(15.0))),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.greenAccent),
-                  borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                SizedBox(
+                  height: 16,
                 ),
-                prefixIcon: Icon(
-                  Icons.enhanced_encryption,
-                  size: 30.0,
+                TextFormField(
+                  controller:_passController,
+                  decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.transparent),
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(15.0))),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.greenAccent),
+                        borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                      ),
+                      prefixIcon: Icon(
+                        Icons.enhanced_encryption,
+                        size: 30.0,
+                      ),
+                      hintText: "Password",
+                      filled: true,
+                      fillColor: Colors.grey[200]),
                 ),
-                hintText: "Password",
-                filled: true,
-                fillColor: Colors.grey[200]),
+                SizedBox(
+                  height: 16,
+                ),
+                Container(
+                  width: double.infinity,
+                  child: FlatButton(
+                    onPressed: (){},
+                    // onPressed: () async {
+                    //   final email = _emailController.text.toString().trim();
+                    //   final pass = _passController.text.toString().trim();
+
+                    //   FirebaseUser user = await login(email, pass);
+
+                    //   if (user != null) {
+                    //     Navigator.of(context).push(MaterialPageRoute(
+                    //         builder: (context) => HomePage(name:user.displayName,imageUrl:user.photoUrl)));
+                    //   }
+                    // },
+                    child: Text("Login"),
+                    textColor: Colors.white,
+                    padding: EdgeInsets.all(16.0),
+                    color: Colors.blue,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                  ),
+                )
+              ],
+            )),
           ),
-         
-          SizedBox(
-            height: 16,
-          ),
-          Container(
-            width: double.infinity,
-            child: FlatButton(
-              onPressed: () {},
-              child: Text("Login"),
-              textColor: Colors.white,
-              padding: EdgeInsets.all(16.0),
-              color: Colors.blue,
-            ),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(15.0)),
-            ),
-          )
-        ],
-              )),
-            ),
-          ),
+        ),
       ),
     );
   }
--- lib/Screens/RegistrationScreen.dart
+++ lib/Screens/RegistrationScreen.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/Screens/LoginScreen.dart';
 
 class RegistrationScreen extends StatelessWidget {
-  final _emailController=TextEditingController();
-  final _passController=TextEditingController();
-  final _nameController=TextEditingController();
-  final _imageController=TextEditingController();
+  final _emailController = TextEditingController();
+  final _passController = TextEditingController();
+  final _nameController = TextEditingController();
+  final _imageController = TextEditingController();
+
+  // Future<bool> registerUser(
+  //     String email, String pass, String name, String url) async {
+  //   FirebaseAuth _auth = FirebaseAuth.instance;
+  //   try {
+  //     AuthResult result = await _auth.createUserWithEmailAndPassword(
+  //         emai: email, password: pass);
+  //     FirebaseUser user = result.User;
+  //     UserUpdateInfo info = UserUpdateInfo();
+  //     info.displayName = name;
+  //     info.photoUrl = url;
+  //     user.updateProfile(info);
+  //     return true;
+  //   } catch (e) {
+  //     print(e);
+  //     return false;
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-              child: SingleChildScrollView(
+        child: SingleChildScrollView(
           child: Container(
             padding: EdgeInsets.all(32),
             child: Form(
@@ -32,7 +51,8 @@ class RegistrationScreen extends StatelessWidget {
                   decoration: InputDecoration(
                       enabledBorder: OutlineInputBorder(
                           borderSide: BorderSide(color: Colors.transparent),
-                          borderRadius: BorderRadius.all(Radius.circular(15.0))),
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(15.0))),
                       focusedBorder: OutlineInputBorder(
                         borderSide: BorderSide(color: Colors.greenAccent),
                         borderRadius: BorderRadius.all(Radius.circular(15.0)),
@@ -53,7 +73,8 @@ class RegistrationScreen extends StatelessWidget {
                   decoration: InputDecoration(
                       enabledBorder: OutlineInputBorder(
                           borderSide: BorderSide(color: Colors.transparent),
-                          borderRadius: BorderRadius.all(Radius.circular(15.0))),
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(15.0))),
                       focusedBorder: OutlineInputBorder(
                         borderSide: BorderSide(color: Colors.greenAccent),
                         borderRadius: BorderRadius.all(Radius.circular(15.0)),
@@ -74,7 +95,8 @@ class RegistrationScreen extends StatelessWidget {
                   decoration: InputDecoration(
                       enabledBorder: OutlineInputBorder(
                           borderSide: BorderSide(color: Colors.transparent),
-                          borderRadius: BorderRadius.all(Radius.circular(15.0))),
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(15.0))),
                       focusedBorder: OutlineInputBorder(
                         borderSide: BorderSide(color: Colors.greenAccent),
                         borderRadius: BorderRadius.all(Radius.circular(15.0)),
@@ -95,7 +117,8 @@ class RegistrationScreen extends StatelessWidget {
                   decoration: InputDecoration(
                       enabledBorder: OutlineInputBorder(
                           borderSide: BorderSide(color: Colors.transparent),
-                          borderRadius: BorderRadius.all(Radius.circular(15.0))),
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(15.0))),
                       focusedBorder: OutlineInputBorder(
                         borderSide: BorderSide(color: Colors.greenAccent),
                         borderRadius: BorderRadius.all(Radius.circular(15.0)),
@@ -114,7 +137,23 @@ class RegistrationScreen extends StatelessWidget {
                 Container(
                   width: double.infinity,
                   child: FlatButton(
-                    onPressed: () {},
+                    onPressed: (){},
+                    // onPressed: () async {
+                    //   final email = _emailController.text.toString().trim();
+                    //   final pass = _passController.text.toString().trim();
+                    //   final name = _nameController.text.toString().trim();
+                    //   final url = _imageController.text.toString().trim();
+
+                    //   bool result = await registerUser(email, pass, name, url);
+
+                    //   if (result) {
+                    //     Navigator.of(context).push(MaterialPageRoute(
+                    //       builder: (context) => LoginScreen(),
+                    //     ));
+                    //   } else {
+                    //     print("error"); 
+                    //   }
+                    // },
                     child: Text("Register"),
                     textColor: Colors.white,
                     padding: EdgeInsets.all(16.0),
